c1356faa96de0f9bda9b23a6b2f0cc3ac461f0a3
slowbeam <sedwardscode@gmail.com>
2019-02-23 10:06:13 -0500

    created api file and added axios interceptors for handling fetch tokens and local storage on request and response

---
+++ src/services/api.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const API_URL = `${process.env.REACT_APP_API_URL}`;
+const LOCAL_STORAGE_KEY = `${process.env.REACT_APP_STORAGE_KEY}`;
+
+const DEFAULT_HEADERS = {
+    "content-type": "application/json"
+};
+
+axios.interceptors.request.use(
+    config => {
+        const {accessToken, clientToken, userToken} = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || {};
+
+        Object.assign(config.headers.common, {
+            "token-type": "Bearer",
+            "access-token": accessToken,
+            "client": clientToken,
+            "uid": userToken
+        });
+        return config;
+    },
+    error => Promise.reject(error)
+)
+
+axios.interceptors.response.use(
+    response => {
+        const accessToken = response.headers['access-totken'];
+        const clientToken = response.headers['client'];
+        const userToken = response.headers['uid'];
+
+        if (accessToken) {
+            localStorage.setItem(
+                LOCAL_STORAGE_KEY, 
+                JSON.stringify({accessToken, clientToken, userToken})
+            )
+        }
+        return response;
+    },
+    error => Promise.reject(error)
+)
